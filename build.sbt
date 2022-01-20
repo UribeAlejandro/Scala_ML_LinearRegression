4f7d50fdeda268b2bbca92e387ccd7d169d3b87d
Alejandro Uribe <jauriberamirez@gmail.com>
2022-01-19 19:56:41 -0500

    Up to training, missing Viz

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Scala_ML_LinearRegression"
+
+version := "0.1"
+
+scalaVersion := "2.12.15"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.0" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.2.0" % "provided"
---
+++ src/main/scala/LinearRegressionExample.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.regression.LinearRegression
+
+object LinearRegressionExample {
+
+  def main(): Unit = {
+    val spark = SparkSession.builder().appName("LinearRegression").getOrCreate()
+
+    val path = "data/sample_linear_reg_data.txt"
+
+    val training = spark.read.format("libsvm").load(path)
+    training.printSchema()
+
+    val lr = new LinearRegression()
+      .setMaxIter(100)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+
+    val lrModel = lr.fit(training)
+
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    val trainingSummary = lrModel.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+
+    spark.stop()
+  }
+
+  main()
+}
